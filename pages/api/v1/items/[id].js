c698b4b437208cc9ea65d2bea08d8120530ae272
Paula Gibrim <paula.gibrim@ufv.br>
2025-10-12 17:57:28 -0300

    feat: add endpoint `DELETE /api/v1/items/[id]`

---
+++ pages/api/v1/items/[id].js
@@ -0,0 +1,48 @@
+import { removeItemById } from '../../../../models/items'
+
+export default async function handler(req, res) {
+
+    // Verifica se o verbo HTTP é delete, e, se não for,
+    // garante que não saia daqui (não faça mais nada)
+    if (req.method !== 'DELETE') {
+        res.setHeader('Allow', 'DELETE');
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+
+    // Le o id da rota (recebe os parâmetros da rota dinâmica)
+    // Ex.: para /api/v1/items/abc, req.query.id === 'abc';
+    // mas para /api/v1/items/a/b, id vem como string[ ]
+    // Por isso, precisa fazer uma normalização
+    const id = req.query;
+
+    // Normaliza o valor, garantindo q o array seja sempre uma
+    // string ou undefined
+    const itemId = Array.isArray(id) ? id[0] : id;
+
+    // console.log(itemId.id);
+
+    // Verificar a presença de um id (se n foi undefined/null/string vazia)
+    if (!itemId.id) {
+        return res.status(400).json({ error: 'Item Id is required' });
+    }
+
+    try {
+        // Chama o modelo para fazer a exclusão, passando o id
+        // true se tiver deletado ok, false se nao encontrou esse id
+        const deleteResult = await removeItemById(itemId.id);
+
+        // Se for not found, erro:
+        if (!deleteResult) {
+            return res.status(404).json({ error: 'Item not found' });
+        }
+
+        // Achei desnecessário uma mensagem aqui, pq só deletamos.
+        // return res.status(200).json({ message: "Item deleted successfully" });
+        return res.status(204).end();
+
+    } catch (error) { // Para pegar algum erro inesperado
+        console.error('DELETE /api/v1/items/:id error', error);
+        return res.status(500).json({ error: 'Something went wrong...' });
+    }
+
+}
