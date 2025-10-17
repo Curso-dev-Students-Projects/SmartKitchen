30d056e98d09ef9008f67cc24eb3389f06d3ad29
Kenny de Souza Vargas <71137658+KennysSparda@users.noreply.github.com>
2025-10-17 13:03:10 -0300

    refactor(validateUUID): rename parameter and adjust formatting
    add line break on line 4
    
    rename parameter in validate uuid function

--- pages/api/v1/items/[id].js
+++ pages/api/v1/items/[id].js
@@ -1,11 +1,12 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import item from "models/item";
+
 const router = createRouter();
 
-export default router.handler(controller.errorHandlers);
-
 router.delete(deleteHandler);
+
+export default router.handler(controller.errorHandlers);
 
 async function deleteHandler(request, response) {
     await item.remove(request.query);
